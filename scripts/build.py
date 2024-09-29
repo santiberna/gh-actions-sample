52fd4f3ce0ebb6f56b4231308b8f4b6f78134eda
Santiago Bernardino <santibberna@gmail.com>
2024-09-29 20:11:46 +0200

    [Fix] .vscode tasks and launch config [Add] Test external library code

--- scripts/build.py
+++ scripts/build.py
@@ -1,8 +1,6 @@
 import sys
 import subprocess
 
-DEBUG = "x64-Debug"
-RELEASE = "x64-Release"
 
 COMPILE_SUCCESS = "Project compilation succeded"
 COMPILE_FAIL = "Project failed to compile"
@@ -12,6 +10,9 @@ CONFIGURE_FAIL = "Failed to configure build files"
 
 def configure(config):
     return subprocess.run(["cmake", "--preset=" + config]).returncode
+
+def clean(config):
+    return subprocess.run(["cmake", "--build", "build/" + config, "--target clean"]).returncode
 
 def build(config):
     return subprocess.run(["cmake", "--build", "build/" + config]).returncode
@@ -26,9 +27,16 @@ def check(code, success_message, fail_message):
 
 def main():
     preset = sys.argv[1]
-    check(configure(preset), CONFIGURE_SUCCESS, CONFIGURE_FAIL)
-    check(build(preset), COMPILE_SUCCESS, COMPILE_FAIL)
+
+    if (preset == "Clean"):
+        clean("Debug")
+        clean("Release")
+    else:
+        check(configure(preset), CONFIGURE_SUCCESS, CONFIGURE_FAIL)
+        check(build(preset), COMPILE_SUCCESS, COMPILE_FAIL)
     
 
 if __name__ == "__main__":
     main()
+
+    # cmake --build C:/foo/build/ --target clean
--- source/main.cpp
+++ source/main.cpp
@@ -2,20 +2,31 @@
 #include <string>
 #include <vector>
 
+#include <vulkan/vulkan.hpp>
+
 int main(int argc, char* argv[])
 {
     std::cout << "Hello World" << std::endl;
 
-    std::vector<std::string> cmd_args;
-
     for (int i = 0; i < argc; i++)
     {
-        cmd_args.emplace_back(std::string(argv[i]));
+        std::cout << argv[i] << std::endl;
     }
 
-    for (int i = 0; i < cmd_args.size(); i++)
+    if (false)
     {
-        std::cout << /*std::format("Arg {}: {}", i, cmd_args[i])*/ cmd_args[i] << std::endl;
+        vk::ApplicationInfo appInfo = {};
+        appInfo.pApplicationName = "My Vulkan Application"; // Application name
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0); // Application version
+        appInfo.pEngineName = "No Engine"; // Engine name (if any)
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0); // Engine version
+        appInfo.apiVersion = VK_API_VERSION_1_2; // The Vulkan API version (1.2 in this case)
+
+        vk::InstanceCreateInfo createInfo = {};
+        createInfo.pApplicationInfo = &appInfo; // Associate the appInfo with the createInfo
+
+        vk::Instance instance = vk::createInstance(createInfo);
+        instance.destroy();
     }
 
     return 0;
